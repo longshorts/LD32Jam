e0c786b21cc71b2d091ce3506859a8258f781d50
Andrew Nash <andrew@andrewoid.co.uk>
2015-04-18 11:54:57 +0100

    Initial commit of libgdx

---
+++ android/src/com/nashdevsoft/ld32jam/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nashdevsoft.ld32jam.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nashdevsoft.ld32jam.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
---
+++ html/src/com/nashdevsoft/ld32jam/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.nashdevsoft.ld32jam.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.nashdevsoft.ld32jam.MainGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MainGame();
+        }
+}
